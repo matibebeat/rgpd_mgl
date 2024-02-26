fa1f90f91d6fdaa1f6defd21999a72e6732c8797
matibebeat <mathislair@gmail.com>
2024-02-26 19:30:07 +0100

    first commit

---
+++ background/background.js
@@ -0,0 +1,7 @@
+//  TODO another endroit to add code
+
+/*
+
+  c'est un script qui est executé en arrière plan, il peux etre appele dans le runer.js
+
+*/
---
+++ runer.js
@@ -0,0 +1,7 @@
+//TODO la partie de l'application qui fait des trucs intelligents
+
+/*
+
+   c'est la partie de l'extension qui est executé a chaque fois que l'utilisateur ouvre une page web
+
+*/
